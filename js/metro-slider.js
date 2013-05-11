53813f56a7650e5d359057fbdd2144d5e1c2fe8f
Jiachi Liu <jiachiliu@Jiachis-Computer.local>
2013-05-11 18:20:12 -0400

    remove pagination in html page
    Remove pagination in html page
    Add code to dynamically create pagination

--- js/metro-slider.js
+++ js/metro-slider.js
@@ -1,11 +1,18 @@
 var MetroSlider={};
 
+/*
+* Set up default parameters for slider.
+*/
 MetroSlider.setupParameters=function(){
-	MetroSlider.sliderWidth=$('.slider').width();
+	MetroSlider.sliderWidth=$('.metroSlider').width();
 	MetroSlider.numOfContents=$('.sliderContent li').length;
 	$('.sliderContent li').eq(0).addClass("active");
 }
 
+/*
+* wrap a div out of slider contents. The width of div is
+* based on the total width of contents.
+*/
 MetroSlider.wrapContent=function(){
 	$(".sliderContent").wrap("<div class='sliderWrap'>");
 	$('.sliderWrap').css({
@@ -13,20 +20,32 @@ MetroSlider.wrapContent=function(){
 	});
 }
 
+/*
+* Set the content indicated by index to active.
+*/
 MetroSlider.setNewDisplay=function(index){
 	$('.active').removeClass('active');
 	$('.sliderContent li').eq(index).addClass("active");
 	$('.paging ul li').eq(index).addClass('active');
 }
 
+/*
+* Get the index of active content.
+*/
 MetroSlider.getCurrentIndex=function(){
 	return $('.active').index();
 }
 
+/*
+* Get the next index of current active element.
+*/
 MetroSlider.getNextIndex=function(index){
 	return (index+1)%MetroSlider.numOfContents;
 }
 
+/*
+* Get the previous index of current active element.
+*/
 MetroSlider.getPreviouIndex=function(index){
 	var pre=index-1;
 	if(pre<0){
@@ -35,6 +54,9 @@ MetroSlider.getPreviouIndex=function(index){
 	return pre%3;
 }
 
+/*
+* Bind the next click event
+*/
 MetroSlider.bindNextEvent=function(){
 	$('.next').click(function(){
 		MetroSlider.bindClickEvent(MetroSlider.next);
@@ -42,6 +64,9 @@ MetroSlider.bindNextEvent=function(){
 	});
 }
 
+/*
+* Bind the previou click event
+*/
 MetroSlider.bindPreviouEvent=function(){
 	$('.pre').click(function() {	
 		MetroSlider.bindClickEvent(MetroSlider.previou);
@@ -49,6 +74,9 @@ MetroSlider.bindPreviouEvent=function(){
 	});
 }
 
+/*
+* Bind the click event on pages
+*/
 MetroSlider.bindPaging=function(){
 	$('.paging ul li').click(function(){	
 		MetroSlider.bindClickEvent(MetroSlider.paging,$(this));
@@ -90,9 +118,32 @@ MetroSlider.autoSwitch=function(){
 	MetroSlider.interval=setInterval("MetroSlider.next()",4000);
 }
 
+MetroSlider.addPagination=function(){
+	var paging=MetroSlider.createPagination();
+	$('.metroSlider').append(paging);
+}
+
+MetroSlider.createPagination=function(){
+	var paging=$('<div>').addClass('paging');
+	var pre=$('<a href="">pre</a>').addClass('pre');
+	var next=$('<a href="">next</a>').addClass('next');
+	var pages=$('<ul>');
+	var numberOfPages=$('.sliderContent').children().length;
+	for (var i = 0; i < numberOfPages; i++) {
+		pages.append("<li>"+(i+1)+"</li>");
+	};
+
+	pages.children().eq(0).addClass('active');
+	paging.append(pre);
+	paging.append(pages);
+	paging.append(next);
+	return paging;
+}
+
 MetroSlider.initial=function() {
 	MetroSlider.setupParameters();
 	MetroSlider.wrapContent();
+	MetroSlider.addPagination();
 	MetroSlider.bindNextEvent();
 	MetroSlider.bindPreviouEvent();
 	MetroSlider.bindPaging();
